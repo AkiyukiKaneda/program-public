76ed848d4471fe2954d0f2fde438631094f0d522
kanedaa <kaneda.akiyuki.0609@gmail.com>
2022-10-31 22:27:15 +0900

    update

--- TestContract.sol
+++ TestContract.sol
@@ -6,86 +6,126 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract TestContract is ERC721, Ownable {
+contract TestContract is ERC721, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     using Counters for Counters.Counter;
 
-  struct ProposalStruct {
-    uint256 id;
-    uint256 amount;
-    uint256 duration;
-    uint256 upvotes;
-    uint256 downvotes;
-    address payable beneficiary;
-    address proposer;
-    address executor;
-    string title;
-    string description;
-    bool passed;
-    bool paid;
-  }
+    struct ProposalStruct {
+        uint256 id;
+        uint256 amount;
+        uint256 duration;
+        uint256 upvotes;
+        uint256 downvotes;
+        address payable beneficiary;
+        address proposer;
+        address executor;
+        string title;
+        string description;
+        bool passed;
+        bool paid;
+    }
 
-  struct VotedStruct {
-   address voter;
-   uint256 timestamp;
-   bool choosen;
-  }
+    struct VotedStruct {
+       address voter;
+       uint256 timestamp;
+       bool voted;
+    }
 
-  Counters.Counter private _nextId;
+    Counters.Counter private _nextId;
+    mapping(uint256 => Counters.Counter) private _totalVote;
 
-  mapping(uint256 => mapping(uint256 =>ProposalStruct)) private proposal;
-  mapping(uint256 => mapping(uint256 => VotedStruct)) private vote;
+    mapping(uint256 => ProposalStruct) private proposal;
+    mapping(uint256 => mapping(uint256 => VotedStruct)) private vote;
 
-  constructor(
-      string memory _name,
-      string memory _symbol
-  ) ERC721(_name, _symbol) {}
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
 
-   function setProposal(
-     uint256 _tokenId,
-     uint256 _id,
-     address _beneficiary,
-     address _proposer,
-     address _executor,
-     string memory _title,
-     string memory _description
-  ) public returns(bool) {
-    require(_nextId.current() == _id);
-    require(_proposer == msg.sender);
+    function getProposal(uint256 _id) external view returns(ProposalStruct memory) {
+        return proposal[_id];
+    }
 
-    address owner = ownerOf(_tokenId);
+    function performProposal(
+        uint256 _tokenId,
+        address _beneficiary,
+        address _proposer,
+        address _executor,
+        string memory _title,
+        string memory _description
+    ) external returns(bool) {
+        require(_proposer == msg.sender);
 
-    require(owner == msg.sender);
+        address owner = ownerOf(_tokenId);
 
-    proposal[_id][_tokenId].id = _id;
-    proposal[_id][_tokenId].duration = block.timestamp + 30 days;
-    proposal[_id][_tokenId].beneficiary = payable(_beneficiary);
-    proposal[_id][_tokenId].proposer = _proposer;
-    proposal[_id][_tokenId].executor = _executor;
-    proposal[_id][_tokenId].title = _title;
-    proposal[_id][_tokenId].description = _description;
+        require(owner == msg.sender);
 
-    _nextId.increment();
+        uint256 _id = _nextId.current();
 
-    return true;
-  }
+        proposal[_id].id = _id;
+        proposal[_id].duration = block.timestamp + 30 days;
+        proposal[_id].beneficiary = payable(_beneficiary);
+        proposal[_id].proposer = _proposer;
+        proposal[_id].executor = _executor;
+        proposal[_id].title = _title;
+        proposal[_id].description = _description;
 
-  function performVote(
-    uint256 _tokenId,
-    uint256 _id,
-    bool _choosen
-  ) public returns(bool) {
-    uint256 _duration = proposal[_id][_tokenId].duration;
-    require( _duration > block.timestamp);
-    address owner = ownerOf(_tokenId);
-    require(owner == msg.sender);
+        _nextId.increment();
 
-    vote[_id][_tokenId].voter = msg.sender;
-    vote[_id][_tokenId].timestamp = block.timestamp;
-    vote[_id][_tokenId].choosen = _choosen;
+        return true;
+    }
 
-    return true;
-  }
+    function performVote(
+        uint256 _tokenId,
+        uint256 _id,
+        bool _vote
+    ) external nonReentrant returns(bool) {
+        uint256 _currentVoteId = _totalVote[_id].current();
+        require(vote[_id][_currentVoteId].voted != true);
+        require(proposal[_id].duration > block.timestamp);
+        address owner = ownerOf(_tokenId);
+        require(owner == msg.sender);
+
+        vote[_id][_currentVoteId].voter = msg.sender;
+        vote[_id][_currentVoteId].timestamp = block.timestamp;
+        vote[_id][_currentVoteId].voted = true;
+
+        if(_vote == true){
+            proposal[_id].upvotes++;
+        } else {
+            proposal[_id].downvotes++;
+        }
+
+        _totalVote[_id].increment();
+
+        return true;
+    }
+
+    function excutor(uint256 _id) external nonReentrant returns(bool) {
+        require(proposal[_id].paid != true);
+        require(block.timestamp > proposal[_id].duration);
+        require(proposal[_id].executor == msg.sender);
+
+        _determination(_id);
+
+        if(proposal[_id].passed) {
+            proposal[_id].paid = true;
+            payable(proposal[_id].beneficiary).transfer(proposal[_id].amount);
+            return true;
+        }
+        return false;
+    }
+
+    function _determination(uint256 _id) internal {
+        require(block.timestamp > proposal[_id].duration);
+
+        if(proposal[_id].upvotes > proposal[_id].downvotes) {
+            proposal[_id].passed = true;
+        } else {
+            proposal[_id].passed = false;
+        }
+    }
 }
